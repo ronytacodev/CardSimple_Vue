a90718d59724d1117a37eb9451b3637ac1bd6d12
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-07 13:17:24 -0500

    we started to create the project and the first steps of the design

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <card-simple />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'; otra forma de importar
+
+import CardSimple from '@/components/CardSimple.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CardSimple
   }
 }
 </script>
---
+++ src/components/CardSimple.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card-simple">
+        <img src="@/assets/landscape.jpg" class="card-simple__img">
+
+        <p class="card-simple__titulo">Título de la tarjeta</p>
+        <p class="card-simple__subtitulo">Subtítulo de la tarjeta</p>
+        <p class="card-simple__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid error nihil
+            perferendis quibusdam est laborum
+            voluptas fugiat architecto eos, consequuntur, mollitia sit ab ea ipsa, quidem ipsam veniam officiis provident.
+        </p>
+
+        <button class="card-simple__btn">Visitar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardSimple'
+}
+</script>
+
+<style lang="css" scoped></style>
