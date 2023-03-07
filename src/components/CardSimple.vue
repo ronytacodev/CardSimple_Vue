0596551fddeafd6e2f2fa63d521dc8b5dc9ada59
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-07 13:42:05 -0500

    finishing the first simple card

--- src/components/CardSimple.vue
+++ src/components/CardSimple.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card-simple">
-        <img src="@/assets/landscape.jpg" class="card-simple__img">
+        <img class="card-simple__img" src="@/assets/landscape.jpg">
 
         <p class="card-simple__titulo">Título de la tarjeta</p>
         <p class="card-simple__subtitulo">Subtítulo de la tarjeta</p>
@@ -19,4 +19,43 @@ export default {
 }
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.card-simple {
+    border: 1px solid;
+    width: 300px;
+    text-align: center;
+    padding-bottom: 1em;
+    margin: auto;
+}
+
+.card-simple__img {
+    width: 100%;
+}
+
+.card-simple__titulo {
+    font-size: 18px;
+    font-weight: bold;
+    color: #2c3e50;
+}
+
+.card-simple__subtitulo {
+    color: #e74c3c;
+    font-size: 14px;
+}
+
+.card-simple__text {
+    font-size: 16px;
+    color: #95a5a6;
+    margin-left: 10px;
+    margin-right: 10px;
+}
+
+.card-simple__btn {
+    padding: 5px 2em 5px 2em;
+    background-color: #27ae60;
+    border: none;
+    color: white;
+    border-radius: 1em;
+    font-size: 1em;
+}
+</style>
